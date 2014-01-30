b9fa4e663e80bd97d9376967bf2e8100451157d8
Justin Walgran <jwalgran@azavea.com>
2014-01-30 10:26:27 -0500

    Force magit to open fullscreen

--- .emacs.d/user.el
+++ .emacs.d/user.el
@@ -206,3 +206,17 @@
 
 ;; bind ace-jump-mode
 (global-set-key (kbd "C-x C-a") 'ace-jump-mode)
+
+;; full screen magit-status (http://whattheemacsd.com/)
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
