b6c9677f807db9196fc3704e1d737e4b16ef61e1
Justin Walgran <jwalgran@azavea.com>
2014-01-31 11:59:48 -0500

    Emacs - Add an insert-date function

--- .emacs.d/user.el
+++ .emacs.d/user.el
@@ -235,3 +235,18 @@
 
 (global-set-key (kbd "C-c O") 'add-line-above)
 (global-set-key (kbd "C-c o") 'add-line-below)
+
+;; Insert Date
+;;
+(defun insert-date (prefix)
+  "Insert the current date. With prefix-argument, use ISO format. With
+   two prefix arguments, write out the day and month name."
+  (interactive "P")
+  (let ((format (cond
+                 ((not prefix) "%Y-%m-%d")
+                 ((equal prefix '(4)) "%A, %B %d %Y")
+                 ((equal prefix '(16)) "%A, %d. %B %Y")))
+        (system-time-locale "en_US"))
+    (insert (format-time-string format))))
+
+(global-set-key (kbd "C-c d") 'insert-date)
