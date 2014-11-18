54b9c6eb1a1026b2052ab73cc3cf6661d98358eb
Justin Walgran <jwalgran@azavea.com>
2014-11-18 11:36:55 -0500

    Emacs - add key bindings for multiple-cursors
    I use this package pretty regularly. These were the suggested bindings
    on https://github.com/magnars/multiple-cursors.el

--- .emacs.d/modes.el
+++ .emacs.d/modes.el
@@ -119,3 +119,13 @@
 
 
 (provide 'modes)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; multiple-cursors
+
+(use-package multiple-cursors
+  :init
+  (progn
+    (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+    (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)))
