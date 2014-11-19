16f8f5601033a24eb7e71558d9a0c0b75ea7bbcf
Justin Walgran <jwalgran@azavea.com>
2014-11-19 12:44:28 -0500

    Emacs - fix multiple cursor key binding syntax
    I had copy pasted an old config rather than rewrite it for the new
    use-package syntax.

--- .emacs.d/modes.el
+++ .emacs.d/modes.el
@@ -123,9 +123,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; multiple-cursors
 
 (use-package multiple-cursors
-  :init
-  (progn
-    (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-    (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-    (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-    (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)))
+  :bind
+  (("C-S-c C-S-c" . mc/edit-lines)
+   ("C->"         . mc/mark-next-like-this)
+   ("C-<"         . mc/mark-previous-like-this)
+   ("C-c C-<"     . mc/mark-all-like-this)))
