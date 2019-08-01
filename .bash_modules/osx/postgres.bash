b55e5696244e161976e0c6ca3dff736014415689
Justin Walgran <jwalgran@azavea.com>
2019-08-01 07:37:16 -0700

    Bash - Update Postgres.app path entry
    I updated the Postgres.app version on my machine and want the latest version of
    the command line tools on the PATH.

--- .bash_modules/osx/postgres.bash
+++ .bash_modules/osx/postgres.bash
@@ -1,3 +1,3 @@
 # TODO: Check if Postgres.app is installed and use whatever version
 # is in /Versions
-export PATH="/Applications/Postgres.app/Contents/Versions/9.5/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/11.4/bin:$PATH"
