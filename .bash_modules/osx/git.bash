f631bec04cb07b6c7913328781cdcb9fd3dc1def
Justin Walgran <jwalgran@azavea.com>
2020-06-01 11:20:26 -0700

    Bash - Update the location of macOS git completion and prompt
    These scripts changes location after an OS update. I am leaving behind the old
    versions so that this script will also work on older macOS versions.

--- .bash_modules/osx/git.bash
+++ .bash_modules/osx/git.bash
@@ -2,7 +2,16 @@ if [ -f /usr/local/etc/bash_completion.d/git-completion.bash ]; then
   . /usr/local/etc/bash_completion.d/git-completion.bash;
 fi
 
+if [ -f /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash ]; then
+    . /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash;
+fi
+
 if [ -f /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh ]; then
   . /Library/Developer/CommandLineTools/usr/share/git-core/git-prompt.sh;
   PS1='[\h \W$(__git_ps1 " (%s)")]\$ ';
+fi
+
+if [ -f /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh ]; then
+    . /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh;
+    PS1='[\h \W$(__git_ps1 " (%s)")]\$ ';
 fi
